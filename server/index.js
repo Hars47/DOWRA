313473967a4b32c8c208674f6315c5c76724bbdb
Hars47 <saharsha2004@gmail.com>
2024-09-12 20:19:35 +0530

    Routing

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dowraRoutes from './routes/dowraRoutes.js';
 
 /*This line allows us to pool our environment variables from DOT Env file which I'll create soon*/
 dotenv.config();
@@ -9,6 +11,10 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+
+/*Basically created API endpoints that we can connect/hook onto from front-end side*/
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dowra', dowraRoutes);
 
 app.get('/', async (req, res) => {
     res.send('Hello from DOWRA');
@@ -24,7 +30,7 @@ const startServer = async () => {
 
     catch (error) {
         console.log(error);
-        
+
     }
 
 }
