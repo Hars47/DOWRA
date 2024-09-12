050fa3930d67ea89a2c329ed0ed9ef8cdb188d97
Hars47 <saharsha2004@gmail.com>
2024-09-12 20:38:49 +0530

    Integrating backend to frontend and added searchChange function

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,11 +12,51 @@ const RenderCards = ({ data, title }) => {
 }
 
 const Home = () => {
-
   const [loading, setLoading] = useState(false);
-  const [allPosts, setAllPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState(null);
 
-  const [searchText, setsearchText] = useState('');
+  const [searchText, setSearchText] = useState('');
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState(null);
+
+  const fetchPosts = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch('https://dowra.onrender.com/api/v1/post', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setAllPosts(result.data.reverse());
+      }
+    } catch (err) {
+      alert(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = allPosts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLowerCase()));
+        setSearchedResults(searchResult);
+      }, 500),
+    );
+  };
+
 
   return (
     <section className="max-w-7xl mx-auto">
